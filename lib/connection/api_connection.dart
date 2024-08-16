62b51c3cdb90fc26dabd59c8dda44b219ada9184
Agustino Whickey <wakembus98@gmail.com>
2024-08-16 10:48:40 +0700

    Set API Connection

---
+++ lib/connection/api_connection.dart
@@ -0,0 +1,8 @@
+class API {
+  static const hostConnect = "http://192.168.8.133/api_clothes_store";
+  static const hostConnectUser = "$hostConnect/user";
+
+  static const validateEmail = "$hostConnect/user/validate_email.php";
+  static const signUp = "$hostConnect/user/signup.php";
+  static const login = "$hostConnect/user/login.php";
+}
